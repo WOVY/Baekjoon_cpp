21c8528c2162d13e49c82f1429950efc20769bd9
WOVY <90702027+WOVY@users.noreply.github.com>
2025-05-17 15:38:37 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool check_Palindrome(string s) { //palindrome인지 확인하는 함수수
+    int i = 0;
+    bool flag = true;
+    while (i < s.length() / 2) {
+        if (s[i] != s[s.length() - 1 - i]) {
+            flag = false;
+            break;
+        }
+        i++;
+    }
+
+    return flag;
+}
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    if (check_Palindrome(s))
+        cout << 1 << endl;
+    else
+        cout << 0 << endl;
+
+    return 0;
+}
